5bfbc68f45d1a78efa2dfa147331ffe539fd0926
Harshil Rathod <50371099+ImHarshil@users.noreply.github.com>
2019-05-22 19:27:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/harshil/fingerprint/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.harshil.fingerprint;
+
+import android.content.Intent;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.multidots.fingerprintauth.AuthErrorCodes;
+import com.multidots.fingerprintauth.FingerPrintAuthCallback;
+import com.multidots.fingerprintauth.FingerPrintAuthHelper;
+
+public class MainActivity extends AppCompatActivity implements FingerPrintAuthCallback
+{
+    private FingerPrintAuthHelper mFingerPrintAuthHelper;
+    private android.widget.ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        mFingerPrintAuthHelper = FingerPrintAuthHelper.getHelper(this, (FingerPrintAuthCallback) this);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //start finger print authentication
+        mFingerPrintAuthHelper.startAuth();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFingerPrintAuthHelper.stopAuth();
+    }
+
+    @Override
+    public void onNoFingerPrintHardwareFound() {
+
+    }
+
+    @Override
+    public void onNoFingerPrintRegistered() {
+
+    }
+
+    @Override
+    public void onBelowMarshmallow() {
+
+    }
+
+    @Override
+    public void onAuthSuccess(FingerprintManager.CryptoObject cryptoObject) {
+        startActivity(new Intent(getApplicationContext(),Main2Activity.class));
+        finish();
+    }
+
+    @Override
+    public void onAuthFailed(int errorCode, String errorMessage) {
+        switch (errorCode) {    //Parse the error code for recoverable/non recoverable error.
+            case AuthErrorCodes.CANNOT_RECOGNIZE_ERROR:
+                Toast.makeText(getApplicationContext(),"unknown user",Toast.LENGTH_LONG).show();
+                //Cannot recognize the fingerprint scanned.
+                break;
+            case AuthErrorCodes.NON_RECOVERABLE_ERROR:
+                //This is not recoverable error. Try other options for user authentication. like pin, password.
+                Toast.makeText(getApplicationContext(),"unknown user",Toast.LENGTH_LONG).show();
+
+                break;
+            case AuthErrorCodes.RECOVERABLE_ERROR:
+                //Any recoverable error. Display message to the user.
+                Toast.makeText(getApplicationContext(),"unknown user",Toast.LENGTH_LONG).show();
+
+                break;
+        }
+    }
+}
